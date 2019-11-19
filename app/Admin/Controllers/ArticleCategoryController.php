cb088fa5ea8bfcf16d28548737ab1a6fccd18602
SuperMind <wtzhang2011@live.cn>
2019-11-19 16:36:38 +0800

    ..

---
+++ app/Admin/Controllers/ArticleCategoryController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\ArticleCategory;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class ArticleCategoryController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('分类管理')
+            ->description('文章分类列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('查看')
+            ->description('查看分类')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('编辑')
+            ->description('修改分类')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('新建')
+            ->description('创建分类')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new ArticleCategory);
+
+        $grid->id('ID');
+        $grid->name('分类名称');
+        $grid->sort('排序');
+        $grid->created_at('创建时间');
+        $grid->updated_at('更新时间');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(ArticleCategory::findOrFail($id));
+
+        $show->id('ID');
+        $show->name('分类名称');
+        $show->sort('排序');
+        $show->created_at('创建时间');
+        $show->updated_at('更新时间');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new ArticleCategory);
+
+        $form->display('ID');
+        $form->text('name','分类名称');
+        $form->number('sort','排序');
+
+        return $form;
+    }
+}
